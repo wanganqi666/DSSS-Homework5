201ef4e9be75a1236d448474d47c4664ced62838
wanganqi666 <117129392+wanganqi666@users.noreply.github.com>
2022-12-02 00:27:59 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name = "snowflake",
+    version = "0.1",
+    author = "Anqi Wang"
+    author_email = "anqi.wang@fau.de",
+    packages = find_packages(),
+    install_requires = ["numpy", "turtles", "random"],
+ )
